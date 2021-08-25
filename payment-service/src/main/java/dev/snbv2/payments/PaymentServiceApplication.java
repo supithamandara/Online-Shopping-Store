087d806325d5e6b0e789ef10c0fce1267a95779b
Buddhika Weerasinghe <54759523+TurboJapuraEfac@users.noreply.github.com>
2021-08-25 22:21:48 +0530

    reuploading files because of a accidental deletion

---
+++ payment-service/src/main/java/dev/snbv2/payments/PaymentServiceApplication.java
@@ -0,0 +1,21 @@
+package dev.snbv2.payments;
+import org.springframework.amqp.core.Queue;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentServiceApplication.class, args);
+	}
+
+	@Bean
+	public Queue queue() {
+		return new Queue("payments");
+	}
+
+}
---
+++ payment-service/src/test/java/dev/snbv2/payments/PaymentMessageServiceTests.java
@@ -0,0 +1,10 @@
+package dev.snbv2.payments;
+
+import org.springframework.amqp.rabbit.test.context.SpringRabbitTest;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringJUnitConfig
+@SpringRabbitTest
+public class PaymentMessageServiceTests {
+    
+}
