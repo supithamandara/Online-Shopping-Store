6a1615b5ee8f28bff4e5d29c76d47c340c6c5048
Malith Kulasinghe <52590872+CJ9516@users.noreply.github.com>
2021-08-25 21:39:19 +0530

    Delete payment-history directory

--- payment-history/src/main/java/dev/snbv2/payments/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.snbv2.payments;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface PaymentRepository extends CrudRepository<Payment, Long> {
-    
-}
--- payment-history/src/test/java/dev/snbv2/payments/PaymentMessageServiceTests.java
+++
@@ -1,8 +0,0 @@
-package dev.snbv2.payments;
-
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-
-@SpringJUnitConfig
-public class PaymentMessageServiceTests {
-    
-}
